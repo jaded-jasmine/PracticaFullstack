32a711c10bbe4a111fddd181e765bd237fd28c7f
Iasmina <jaded.jasmine@proton.me>
2025-07-24 12:37:44 +0300

    Added DatabaseContext.cs

---
+++ PracticaFullstack/Context/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticaFullstack.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PracticaFullstack.Context
+{
+
+    [Table("cats")]
+    public class DatabaseContext : DbContext
+    {
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cat> Cats { get; set; }
+
+    }
+}
+
