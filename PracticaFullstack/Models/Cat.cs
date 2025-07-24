fffd4093d7421cd099be38844d86dd178c4a183b
Iasmina <jaded.jasmine@proton.me>
2025-07-24 12:38:47 +0300

    Modified Cat.cs

--- PracticaFullstack/Models/Cat.cs
+++ PracticaFullstack/Models/Cat.cs
@@ -7,4 +7,13 @@
         public int? Age{ get; set; }
         public string? Breed { get; set; }
     }
+
+    public class Category
+    {
+        public int Id { get; set; } 
+        public required string Name { get; set; }
+
+        public ICollection<Cat> Products { get; set; } = new List<Cat>();
+
+    }
 }
